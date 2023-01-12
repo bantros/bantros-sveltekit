0f17e328333c2e1b6757957fbcee3655f833c696
Jonathon Halliwell <jon@bantros.net>
2023-01-12 23:44:31 +0000

    Create Player component

---
+++ src/components/Player.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let playing: any;
+</script>
+
+<div class="p-8  md:p-16">
+	<div class="max-w-2xl">
+		<h2 class="mb-2 text-base text-white/60 md:text-xl">
+			{playing.is_playing ? 'Now Playing' : 'Recently Played'}
+		</h2>
+		{#if playing?.progress_ms}
+			<p class="text-2xl md:text-4xl lg:text-5xl mb-2">
+				{playing.item.name}
+				{#if playing.item.artists[0].name !== null}
+					<span class="text-white/30">{playing.item.artists[0].name}</span>
+				{/if}
+			</p>
+		{:else}
+			<p class="text-2xl md:text-4xl lg:text-5xl mb-2">
+				{playing.items[0].track.name}
+				{#if playing.items[0].track.album.artists[0].name !== null}
+					<span class="text-white/30">{playing.items[0].track.album.artists[0].name}</span>
+				{/if}
+			</p>
+		{/if}
+	</div>
+</div>
