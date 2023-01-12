ae0b9c82781af368dde12cf90dc0935d6c09e204
Jonathon Halliwell <jon@bantros.net>
2023-01-12 23:43:43 +0000

    Update server route with call to Spotify Get Currently Playing Track Web API

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,19 +1,57 @@
-import { TWITTER_BEARER_TOKEN } from '$env/static/private';
+import {
+	SPOTIFY_CLIENT_ID,
+	SPOTIFY_CLIENT_SECRET,
+	SPOTIFY_REFRESH_TOKEN,
+	TWITTER_BEARER_TOKEN
+} from '$env/static/private';
 import type { PageLoad } from './$types';
 
-const userId = '18880560';
-const url = `https://api.twitter.com/2/users/${userId}/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=5`;
+import qs from 'qs';
 
 export const load = (async ({ fetch }) => {
-	const res = await fetch(url, {
-		method: 'GET',
-		headers: {
-			authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
-			'Content-Type': 'application/json'
-		}
-	});
-	const userTweets = await res.json();
+	const basic = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
+
+	const getAccessToken = async () => {
+		const res = await fetch('https://accounts.spotify.com/api/token', {
+			method: 'POST',
+			headers: {
+				Authorization: `Basic ${basic}`,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: qs.stringify({
+				grant_type: 'refresh_token',
+				refresh_token: SPOTIFY_REFRESH_TOKEN
+			})
+		});
+		return res.json();
+	};
+
+	const getNowPlaying = async () => {
+		const { access_token } = await getAccessToken();
+		const res = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+			headers: {
+				Authorization: `Bearer ${access_token}`
+			}
+		});
+		return res.json();
+	};
+
+	const getTimeline = async () => {
+		const res = await fetch(
+			`https://api.twitter.com/2/users/18880560/tweets?tweet.fields=created_at&expansions=author_id&user.fields=created_at&max_results=5`,
+			{
+				method: 'GET',
+				headers: {
+					authorization: `Bearer ${TWITTER_BEARER_TOKEN}`,
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+		return res.json();
+	};
+
 	return {
-		userTweets
+		playing: await getNowPlaying(),
+		timeline: await getTimeline()
 	};
 }) satisfies PageLoad;
